7b566ed845eb76fc2a3ab5deab0460c7283db5d3
Ahrorbek05 <akramjonovahrorbek@gmail.com>
2024-05-18 22:48:43 +0500

    uy vazifa

---
+++ js/main.js
@@ -0,0 +1,153 @@
+// 1-masala
+// let a = 5;
+// if (a > 0) {
+//     a++;
+// }
+// console.log(a);
+
+// 2-masala
+// let n = 5;
+// if ( n > 0) {
+//     n++;
+// } else {
+//     n -= 2;
+// }
+// console.log(n);
+
+// 3-masala
+// let c = 5;
+// if ( c > 0){
+//     c++; 
+// } if ( c < 0){
+//     c -= 2;
+// } if ( c == 0){
+//     10;
+// }
+// console.log(c);
+
+// 4-masala
+// function countPositiveNumbers(a, b, c) {
+//     let count = 0;
+//     if (a > 0) {
+//         result++;
+//     }
+//     if (b > 0) {
+//         result++;
+//     }
+//     if (c > 0) {
+//         result++;
+//     }
+//     return result;
+// }
+// let num1 = 5;
+// let num2 = -3;
+// let num3 = 0;
+// console.log("Kattaroq butun son:", kattaroqButunSon(num1, num2, num3));
+
+// 5-masala
+// let negative = 0;
+// let positive = 0;
+// let a = + prompt("son kiriting");
+// if (a > 0) {
+//     positive++;
+// } else {
+//     negative++;
+// }
+// let b = + prompt("son kiriting");
+// if (b > 0) {
+//     musbat++;
+// } else {
+//     negative++
+// }
+// let c = + prompt("son kiriting");
+// if (c > 0) {
+//     positive++;
+// } else {
+//     negative++;
+// }
+
+// 6-masala
+// Function to determine the larger of two integers
+// function kattaroqButunSon(a, b) {
+//     if (a > b) {
+//         return a;
+//     } else if (b > a) {
+//         return b;
+//     } else {
+//         return "Ikkisi ham teng";
+//     }
+// }
+// let num1 = 10;
+// let num2 = 20;
+
+// let result = kattaroqButunSon(num1, num2);
+// console.log("Kattaroq butun son:", result);
+
+// 7-masala
+// function kichikSon(num1, num2) {
+//     if (num1 < num2) {
+//         return 1; 
+//     } else if (num2 < num1) {
+//         return 2;
+//     } else {
+//         return 0; 
+//     }
+// }
+
+// let num1 = 10;
+// let num2 = 20;
+
+// let result = kichikSon(num1, num2);
+
+// if (result === 0) {
+//     console.log("ikkisi ham teng sonlar.");
+// } else {
+//     console.log(`kichik raqam ${result}.`);
+// }
+
+// 8-masala
+// function displayOrderedIntegers(num1, num2) {
+//     if (num1 > num2) {
+//         console.log(`katta: ${num1}, kichik: ${num2}`);
+//     } else if (num2 > num1) {
+//         console.log(`katta: ${num2}, kichik: ${num1}`);
+//     } else {
+//         console.log(`ikkisi ham teng: ${num1}`);
+//     }
+// }
+// let num1 = 10;
+// let num2 = 20;
+
+// 9-masala
+// function buyurtmaniKorsatish(a, b) {
+//     if (a > b) {
+//         let temp = a;
+//         a = b;
+//         b = temp;
+//     }
+    
+//     console.log(`A (kichik son): ${a}, B (katta son): ${b}`);
+// }
+// let A = 20.5;
+// let B = 10.3;
+
+// buyurtmaniKorsatish(A, B);
+
+// 10-masala
+function absorb_values (A, B);
+if (A !== B);{
+    return A;
+} else {
+
+}
+
+# Example usage:
+A, B = 3, 5
+A, B = absorb_values(A, B)
+print(f"A: {A}, B: {B}")  # Outputs: A: 8, B: 8
+
+A, B = 4, 4
+A, B = absorb_values(A, B)
+print(f"A: {A}, B: {B}")  # Outputs: A: 0, B: 0
+
+
